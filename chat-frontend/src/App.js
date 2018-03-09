e1f909491112993c7ee29fd7039bb2121871ebdd
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-09 18:29:10 +0530

    created home route

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from "./components/home-component/home";
-import './App.css';
+import Login from "./components/login-component/login";
+
 
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={Login} />
+        <Route exact path='/home' component={Home} />
       </Switch>
     );
   }
