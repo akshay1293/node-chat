1b973df4d60f2bb0a7f31de837534d68e8f152c0
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-09 18:29:37 +0530

    added browser history

--- chat-frontend/src/index.js
+++ chat-frontend/src/index.js
@@ -5,9 +5,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const customHistory = createBrowserHistory();
+
 ReactDOM.render(
 
-    <BrowserRouter>
+    <BrowserRouter history={customHistory}>
         <App />
     </BrowserRouter>
 
