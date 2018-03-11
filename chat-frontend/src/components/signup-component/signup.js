a957eac398bcb8d9bd670f0da4050ca18c8dccc2
akshay1293 <akshayaggarwal1293@gmail.com>
2018-03-11 15:55:57 +0530

    singup component created

---
+++ chat-frontend/src/components/signup-component/signup.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Cookies from 'universal-cookie';
+import "../../stylesheet/styles.css";
+
+export default class Signup extends Component {
+
+    render() {
+
+        return (
+
+            <div>
+                signup
+            </div>
+        );
+    }
+}
