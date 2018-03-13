3c0230a4448e4b578357af62d9fa632ed0098df2
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-13 18:33:20 +0530

    added newly created chat reducer to store

--- chat-frontend/src/redux/reducers/index.js
+++ chat-frontend/src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import userRed from './userReducer';
+import chatRed from './chatReducer';
 
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({ userRed });
+const rootReducer = combineReducers({ userRed ,chatRed});
 
 export default rootReducer;
