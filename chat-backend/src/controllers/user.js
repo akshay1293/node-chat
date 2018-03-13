06ea1c7ce20afa1e9e0265554397bbbc2a760cd7
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-13 11:32:57 +0530

    online feature and signout feature added

--- chat-backend/src/controllers/user.js
+++ chat-backend/src/controllers/user.js
@@ -21,7 +21,8 @@ function create(req, res, next) {
 
                         email: req.body.email,
                         handle: req.body.handle,
-                        password: req.body.password
+                        password: req.body.password,
+                        online: false,
                     }, function (err, result) {
 
                         if (!err) {
@@ -53,6 +54,7 @@ function login(req, res, next) {
         .then(function (user) {
 
             if (user) {
+                toggleOnlineStatus(user);
                 var token = jwt.sign({ user: { id: user.id, handle: user.handle, email: user.email } }, config.secret, { expiresIn: 86400 });
                 res.status(200).json({ success: true, token: token, id: user.id, handle: user.handle, email: user.email });
             } else {
@@ -63,6 +65,48 @@ function login(req, res, next) {
 
             console.log(err);
         })
+}
+
+function list(req, res, next) {
+    const { limit = 50, skip = 0 } = req.query;
+    var token = req.headers['x-access-token'];
+    if (!token) {
+
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+    } else {
+
+        jwt.verify(token, config.secret, function (err, decoded) {
+
+            if (err) {
+
+                return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+            }
+
+            User.find()
+                .skip(skip)
+                .limit(limit)
+                .exec()
+                .then((users) => res.json(users))
+        })
+    }
+}
+
+function signOut(req, res, next) {
+
+
+    User.findOne(
+        {
+            handle: req.query.handle,
+        }, function (err, user) {
+
+            if (!err) {
+
+                toggleOnlineStatus(user);
+                res.json({ signOut: true, msg: "success" });
+            }
+        })
+
 }
 
 function authenticate(req, res, next) {
@@ -86,9 +130,11 @@ function validateUser(user) {
     if (!emailPattern.test(user.email)) {
 
         return { valid: false, msg: "Email Invalid" };
+
     } else if (user.password.length < 6) {
 
         return { valid: false, msg: "password too short" };
+
     } else if (user.password != user.confirmPassword) {
 
         return { valid: false, msg: "passwords do not match" };
@@ -100,6 +146,24 @@ function validateUser(user) {
 
 }
 
+function toggleOnlineStatus(user) {
+
+    let query = { handle: user.handle };
+    let update = { online: !user.online };
+
+    User.update(query, update, function (err, res) {
+
+        if (err) {
+
+            console.log("cannot change online status" + err)
+        } else {
+
+            // console.log(res);
+        }
+    })
+
+}
 
 
-module.exports = { create, login, authenticate };
+
+module.exports = { create, login, authenticate, list, signOut };
