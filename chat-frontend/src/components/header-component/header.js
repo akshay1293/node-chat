411e83b378c902ec6e9da8be3a8923ab9c0ca473
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-13 18:28:59 +0530

    popup menu created

--- chat-frontend/src/components/header-component/header.js
+++ chat-frontend/src/components/header-component/header.js
@@ -2,31 +2,48 @@ import React, { Component } from 'react';
 import "../../stylesheet/styles.css";
 import Cookies from 'universal-cookie';
 import Config from '../../config';
+import { connect } from 'react-redux';
 
-
-export default class Header extends Component {
+class Header extends Component {
 
     constructor(props) {
 
         super();
         this.cookie = new Cookies();
         this.config = new Config();
+        this.state = {
+
+            displayMenu: false,
+        }
     }
     render() {
 
+        let menuContainer = {
+
+            display: this.state.displayMenu ? "flex" : "none",
+            position: "absolute",
+            background: "rgba(255, 255, 255, 0.7)",
+            width: "150px",
+            right: "24px",
+            top: "45px",
+            borderRadius: "5px 0px 5px 5px",
+            border: "1px solid darkgray",
+            animationName: "slide",
+            animationDuration: "0.1s"
+        }
         return (
 
             <div className="top">
-                <div className="menu-container">
-
+                <div style={menuContainer}>
+                    <button onClick={this.signOut.bind(this)} className="login-button sign-out">SIGN OUT</button>
                 </div>
                 <div className="top-left">
-                    <p>App Name</p>
+                    <p>{this.props.chatRed.connection.to ? this.props.chatRed.connection.to : ''}</p>
                 </div>
                 <div className="top-right">
                     <span><i className="far fa-user" style={{ fontSize: 14, color: "#FFF", marginRight: "8px" }}></i></span>
                     <div><a style={{ fontSize: 14 }} href="#">{this.props.user != null ? this.props.user.handle : ""}</a></div>
-                    <div className="sign-out" onClick={this.signOut.bind(this)}>
+                    <div className="sign-out" onClick={() => this.setState({ displayMenu: !this.state.displayMenu })}>
                         <span><i className="fas fa-chevron-circle-down" style={{ fontSize: 14, color: "#FFF", marginLeft: "8px" }}></i></span>
                     </div>
                 </div>
@@ -43,10 +60,6 @@ export default class Header extends Component {
             headers: {
                 'content-type': 'application/json'
             },
-            // body: JSON.stringify({
-            //     handle: this.props.user.user.handle,
-
-            // }),
         })
             .then((response) => { return response.json() })
             .then((responsejson) => {
@@ -63,4 +76,8 @@ export default class Header extends Component {
             })
     }
 }
+
+export default connect(({ chatRed }) => ({ chatRed }), {
+
+})(Header);
 
