84b77627277aef5dc8e23732056b3e9f961176b3
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-21 15:01:13 +0530

    getting messages dynamically from conversation

--- chat-frontend/src/components/chatDisplay-component/chatDisplay.js
+++ chat-frontend/src/components/chatDisplay-component/chatDisplay.js
@@ -3,6 +3,8 @@ import Message from '../message-component/message';
 import "../../stylesheet/styles.css";
 import io from "socket.io-client";
 import Config from '../../config';
+import { connect } from 'react-redux';
+import { appendMessage, setConnection } from '../../redux/actions';
 
 
 
@@ -21,22 +23,49 @@ import Config from '../../config';
 // });
 
 
-export default class ChatDisplay extends Component {
+class ChatDisplay extends Component {
+
+
+    componentDidMount() {
+
+        const { socket, appendMessage, setConnection } = this.props;
+
+        socket.on('msg', function (data) {
+
+            appendMessage({ owner: data.from, message: data.msg });
+            setConnection({ to: data.from, from: data.to });
+            localStorage.setItem("connection", JSON.stringify({ to: data.from, from: data.to }));
+
+
+        })
+    }
 
 
 
     render() {
 
+
         return (
 
             <div className="chat-display" id="chat-display">
-                <Message msg={"hello my name is akshay"} />
-                <Message msg={"hello my"} />
-                <Message msg={"hello my name is"} />
-                <Message msg={"hello my name "} />
-                <Message msg={"hello my name is akshay"} />
+
+                {this.renderMessages()}
             </div>
         );
     }
+
+    renderMessages() {
+
+        if (this.props.chatRed.messages.length != 0) {
+
+            return this.props.chatRed.messages.map((message, index) => {
+
+                return <Message msg={message} key={index} />
+            })
+        }
+    }
 }
 
+export default connect(({ userRed, chatRed }) => ({ userRed, chatRed }), {
+    appendMessage, setConnection
+})(ChatDisplay);
