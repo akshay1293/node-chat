d0daf2d920b60f35cff87a4f44fa2a9242e63bd6
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-21 15:07:06 +0530

    some changes requires to make chat feature work

--- chat-frontend/src/redux/reducers/chatReducer.js
+++ chat-frontend/src/redux/reducers/chatReducer.js
@@ -1,6 +1,6 @@
 import { APPEND_MESSAGE, SET_CONNECTION } from '../constants/actionType';
 
-const INITIAL_STATE = { messages: [{ owner: null, message: null }], connection: { to: null, from: null } };
+const INITIAL_STATE = { messages: [], connection: { to: null, from: null } };
 
 export default (state = INITIAL_STATE, action) => {
 
@@ -9,7 +9,7 @@ export default (state = INITIAL_STATE, action) => {
         case SET_CONNECTION:
             return { ...state, connection: { to: action.payload.to, from: action.payload.from } }
         case APPEND_MESSAGE:
-            return { ...state, messages: state.messages.push(action.payload.data) }
+            return { ...state, messages: state.messages.concat(action.payload) }
         default:
             return { ...state }
     }
