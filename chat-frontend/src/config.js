b5dc39bef183d85205c8c2a26388e6cb56bbeee8
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-27 10:32:49 +0530

    server port changed

--- chat-frontend/src/config.js
+++ chat-frontend/src/config.js
@@ -3,11 +3,11 @@ export default class Config {
     getUrl(endpoint = '') {
 
         if (endpoint) {
-            return "http://172.18.3.99:3004/" + endpoint;
+            return "http://172.18.3.99:3005/" + endpoint;
 
         } else {
 
-            return "http://172.18.3.99:3004";
+            return "http://172.18.3.99:3005";
         }
     }
 }
