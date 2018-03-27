4a1fa3bf0cb5044b637e28314a189e66ab12fd9f
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-27 10:34:34 +0530

    new component created info.js

---
+++ chat-frontend/src/components/info-component/info.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class Info extends Component {
+
+    render() {
+
+        return (
+
+            <div style={{ display: 'flex', flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                <i class="fas fa-envelope" style={{ fontSize: 200 }}></i>
+                <h2 style={{ marginTop: 0 }}>Welcome</h2>
+                <p>This is a chat platform , you can select any user from left side to start chat.</p>
+                <h4 style={{ marginTop: 0 }}>Have fun ;)</h4>
+            </div>
+        );
+    }
+}
