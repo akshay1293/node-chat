2d6788b1d4f07bbf89213b80bac1c5bca5971ea5
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-28 18:41:07 +0530

    action to create conversation added

--- chat-frontend/src/redux/constants/actionType.js
+++ chat-frontend/src/redux/constants/actionType.js
@@ -1,3 +1,4 @@
 export const SET_USER = 'set_user';
 export const APPEND_MESSAGE = 'append_message';
+export const CREATE_CONVERSATION = 'createConversation';
 export const SET_CONNECTION = 'setConnection';
