17ccb00c3b4d04468b92fea389954ce9722bc1d7
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-03-30 17:10:22 +0530

    new mongo instance created

--- chat-backend/config.js
+++ chat-backend/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    db: "mongodb://akshayag:akshay1293@ds263988.mlab.com:63988/chat_db",
+    db: "mongodb://akshayag:akshay1293@ds213239.mlab.com:13239/chat_db",
     port: 3005,
     secret: "disizdsekret",
 }
