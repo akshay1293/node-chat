830c0fb435fb722b190f5cf2fe512099f5121242
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-05-25 16:43:38 +0530

    sending mail to confirm account also

--- chat-backend/src/controllers/email.js
+++ chat-backend/src/controllers/email.js
@@ -8,7 +8,7 @@ var config = require('../../config');
 
 class Email {
 
-    static sendTo(to) {
+    static sendTo(to, type) {
 
         var options = {
             auth: {
@@ -19,13 +19,21 @@ class Email {
 
         var client = nodemailer.createTransport(sgTransport(options));
         var token = jwt.sign({ email: to }, config.secret, { expiresIn: '1h' });
+        var html;
+        if (type === "reset") {
+
+            html = '<p>Hi,<br><br>Looks like you requested a new password<br>If that sounds right click the below link to enter a new passsword<br><br><a href="http://localhost:3000/resetpassword?token=' + token + '">reset password</a></p><p><b>This link is valid for 1 hour only</b></p>'
+        } else {
+
+            html = '<p>Hi,<br><br>Welcome to node chat application<br>If that sounds right click the below link to confirm your email<br><br><a href="http://localhost:3005/confirmAccount?token=' + token + '">confirm</a></p><p><b>This link is valid for 1 hour only</b></p>'
+        }
 
         var email = {
             from: 'support@node-chat.com',
             to: to,
             subject: 'Account Confirmation',
             //text: 'Your chat account has been created succesfully',
-            html: '<p>Hi,<br><br>Looks like you requested a new password<br>If that sounds right click the below link to enter a new passsword<br><br><a href="http://localhost:3000/resetpassword?token=' + token + '">reset password</a></p><p><b>This link is valid for 1 hour only</b></p>'
+            html: html,
         };
 
         return new Promise(function (resolve, reject) {
