4ed8aabbee53fbccfc8e6398018ea9d34c953db9
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-05-28 12:35:46 +0530

    some routes changed

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import Home from "./components/home-component/home";
 import Login from "./components/login-component/login";
 import Signup from "./components/signup-component/signup";
-import InputEmail from './components/reset-password-component/inputEmail';
+import InputUsername from './components/reset-password-component/inputUsername';
 import Resetpassword from './components/reset-password-component/resetPassword';
 import { Provider } from 'react-redux';
 import store from './redux/store/configureStore';
@@ -19,7 +19,7 @@ class App extends Component {
           <Route exact path='/home' component={Home} />
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/resetpassword' component={Resetpassword} />
-          <Route exact path='/registeredemail' component={InputEmail} />
+          <Route exact path='/inputUsername' component={InputUsername} />
         </Switch>
       </Provider>
     );
