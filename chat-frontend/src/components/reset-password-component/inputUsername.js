2db560aa70eda6415abb242703e51d45c7084e8c
akshay1293 <akshay.aggarwal@daffodilsw.com>
2018-06-05 15:58:54 +0530

    connot access signup and input username routes if user logged in

--- chat-frontend/src/components/reset-password-component/inputUsername.js
+++ chat-frontend/src/components/reset-password-component/inputUsername.js
@@ -13,49 +13,105 @@ export default class InputUsername extends Component {
         this.state = {
 
             username: null,
+            isLoggedin: true,
         }
         this.config = new Config();
+        this.cookie = new Cookies();
+    }
+
+    componentDidMount() {
+
+
+
+        var token = this.cookie.get("chat_token");
+
+        if (token) {
+
+            fetch(this.config.getUrl('verify'), {
+
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': token,
+                },
+            })
+                .then((response) => { return response.json() })
+                .then((responsejson) => {
+
+                    if (!responsejson.auth) {
+
+
+
+                        this.setState({
+
+                            isLoggedin: false,
+                        })
+                    } else {
+
+                        window.location = this.config.baseUrl + '/home';
+                    }
+                }).catch((err) => {
+
+                    console.log(err);
+                })
+        } else {
+
+            this.setState({
+
+                isLoggedin: false,
+            })
+        }
+
+
     }
 
     render() {
 
-        return (
+        if (!this.state.isLoggedin) {
 
-            <div className="login-container">
-                <img id="loader" src={loader} className="loader" />
-                <div className="login-area-container" id="login-area-container">
-                    <div id="login-area" className="login-area">
-                        <div className="login-head-container"><p className="login-head">Enter Username</p></div>
-                        <strong id="error-msg"></strong>
-                        <div className="input-container">
-                            <span><i className="far fa-user-circle" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
-                            <input type="text" className="input-box" id="username" placeholder="username"
+            return (
 
-                                onChange={(e) => {
-                                    this.setState(
-                                        {
-                                            username: e.target.value,
-                                        })
+                <div className="login-container">
+                    <img id="loader" src={loader} className="loader" />
+                    <div className="login-area-container" id="login-area-container">
+                        <div id="login-area" className="login-area">
+                            <div className="login-head-container"><p className="login-head">Enter Username</p></div>
+                            <strong id="error-msg"></strong>
+                            <div className="input-container">
+                                <span><i className="far fa-user-circle" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
+                                <input type="text" className="input-box" id="username" placeholder="username"
 
-                                }}
+                                    onChange={(e) => {
+                                        this.setState(
+                                            {
+                                                username: e.target.value,
+                                            })
+
+                                    }}
 
 
-                            />
-                            <span id="error-user"><i className="fas fa-times" style={{ fontSize: 18, color: "#E73A4C", marginRight: "5px" }}></i></span>
+                                />
+                                <span id="error-user"><i className="fas fa-times" style={{ fontSize: 18, color: "#E73A4C", marginRight: "5px" }}></i></span>
+                            </div>
+                            <button className="login-button" onClick={this.sendPasswordResetMail.bind(this)} style={{ width: "100%" }}>SUBMIT</button>
+
+
                         </div>
-                        <button className="login-button" onClick={this.sendPasswordResetMail.bind(this)} style={{ width: "100%" }}>SUBMIT</button>
-
-
-                    </div>
-                    <div className="login-foot">
-                        <div className="footer-signup">
-                            <p>Don't have an account?</p>
-                            <a href="/signup">Sign up</a>
+                        <div className="login-foot">
+                            <div className="footer-signup">
+                                <p>Don't have an account?</p>
+                                <a href="/signup">Sign up</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+
+            return <img id="loader" src={loader} className="loader-visible" />
+        }
+
+
     }
 
     sendPasswordResetMail() {
@@ -76,7 +132,7 @@ export default class InputUsername extends Component {
                 })
                 .then((responseJson) => {
 
-                   
+
                     document.getElementById('loader').style.display = "none";
                     document.getElementById('login-area-container').style.filter = "blur(0px)";
                     if (!responseJson.success) {
--- chat-frontend/src/components/signup-component/signup.js
+++ chat-frontend/src/components/signup-component/signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "../../stylesheet/styles.css";
 import Config from '../../config';
 import loader from '../../gif/ajax-loading.gif'
+import Cookie from 'universal-cookie';
 
 
 export default class Signup extends Component {
@@ -19,87 +20,136 @@ export default class Signup extends Component {
             confirmPassword: null,
             error: null,
             success: null,
+            isLoggedin: true,
         }
 
         this.config = new Config();
+        this.cookie = new Cookie();
     }
 
     componentDidMount() {
 
 
+
+        var token = this.cookie.get("chat_token");
+
+        if (token) {
+
+            fetch(this.config.getUrl('verify'), {
+
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': token,
+                },
+            })
+                .then((response) => { return response.json() })
+                .then((responsejson) => {
+
+                    if (!responsejson.auth) {
+
+
+
+                        this.setState({
+
+                            isLoggedin: false,
+                        })
+                    } else {
+
+                        window.location = this.config.baseUrl + '/home';
+                    }
+                }).catch((err) => {
+
+                    console.log(err);
+                })
+        } else {
+
+            this.setState({
+
+                isLoggedin: false,
+            })
+        }
+
+
     }
 
     render() {
+        if (!this.state.isLoggedin) {
 
-        return (
+            return (
 
-            <div className="login-container">
-                <img id="loader" src={loader} className="loader" />
-                <div className="login-area-container" id="login-area-container">
-                    <div id="login-area" className="login-area">
-                        <div className="login-head-container"><p className="login-head">Create New Account</p></div>
-                        <strong id="error-msg"></strong>
-                        <div className="input-container">
-                            <span><i className="far fa-envelope" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
-                            <input type="text" onChange={() => {
+                <div className="login-container">
+                    <img id="loader" src={loader} className="loader" />
+                    <div className="login-area-container" id="login-area-container">
+                        <div id="login-area" className="login-area">
+                            <div className="login-head-container"><p className="login-head">Create New Account</p></div>
+                            <strong id="error-msg"></strong>
+                            <div className="input-container">
+                                <span><i className="far fa-envelope" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
+                                <input type="text" onChange={() => {
 
-                                this.setState({ email: document.getElementById("email").value })
-                            }}
-                                className="input-box" id="email" placeholder="Email"
-                            />
-                        </div>
-                        <div className="input-container">
-                            <span><i className="far fa-user" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
-                            <input type="text" onChange={() => {
-
-                                this.setState({ handle: document.getElementById("username").value });
-
-                            }} className="input-box" id="username" placeholder="Username"
-                            />
-                        </div>
-                        <div className="input-container">
-                            <span><i className="fa fa-venus-mars" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
-                            <select name="gender" id="gender" className="input-box"
-                                onChange={(e) => {
-
-                                    this.setState({ gender: e.target.value });
-
+                                    this.setState({ email: document.getElementById("email").value })
                                 }}
-                            >
-                                <option value="" selected="selected">None</option>
-                                <option value="m">Male</option>
-                                <option value="f">Female</option>
-                            </select>
+                                    className="input-box" id="email" placeholder="Email"
+                                />
+                            </div>
+                            <div className="input-container">
+                                <span><i className="far fa-user" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
+                                <input type="text" onChange={() => {
+
+                                    this.setState({ handle: document.getElementById("username").value });
+
+                                }} className="input-box" id="username" placeholder="Username"
+                                />
+                            </div>
+                            <div className="input-container">
+                                <span><i className="fa fa-venus-mars" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
+                                <select name="gender" id="gender" className="input-box"
+                                    onChange={(e) => {
+
+                                        this.setState({ gender: e.target.value });
+
+                                    }}
+                                >
+                                    <option value="" selected="selected">None</option>
+                                    <option value="m">Male</option>
+                                    <option value="f">Female</option>
+                                </select>
+                            </div>
+                            <div className="input-container">
+                                <span><i className="fas fa-key" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
+                                <input type="password" onChange={() => {
+
+                                    this.setState({ password: document.getElementById("password").value })
+
+                                }} className="input-box" id="password" placeholder="Password"
+                                />
+                            </div>
+                            <div className="input-container">
+                                <span><i className="fas fa-key" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
+                                <input type="password" onChange={() => {
+
+                                    this.setState({ confirmPassword: document.getElementById("confirmPassword").value })
+
+                                }} className="input-box" id="confirmPassword" placeholder="Confirm Password "
+                                />
+                            </div>
+                            <button onClick={this.signup.bind(this)} id="signup" className="login-button" style={{ width: "100%" }}>SIGN UP</button>
                         </div>
-                        <div className="input-container">
-                            <span><i className="fas fa-key" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
-                            <input type="password" onChange={() => {
-
-                                this.setState({ password: document.getElementById("password").value })
-
-                            }} className="input-box" id="password" placeholder="Password"
-                            />
-                        </div>
-                        <div className="input-container">
-                            <span><i className="fas fa-key" style={{ fontSize: 16, color: "#FFF", marginRight: "5px" }}></i></span>
-                            <input type="password" onChange={() => {
-
-                                this.setState({ confirmPassword: document.getElementById("confirmPassword").value })
-
-                            }} className="input-box" id="confirmPassword" placeholder="Confirm Password "
-                            />
-                        </div>
-                        <button onClick={this.signup.bind(this)} id="signup" className="login-button" style={{ width: "100%" }}>SIGN UP</button>
-                    </div>
-                    <div className="login-foot">
-                        <div className="footer-signup">
-                            <p>Click to</p>
-                            <a href="/">Login</a>
+                        <div className="login-foot">
+                            <div className="footer-signup">
+                                <p>Click to</p>
+                                <a href="/">Login</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+
+            return <img id="loader" src={loader} className="loader-visible" />
+        }
+
     }
 
     signup() {
